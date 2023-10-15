d263ae9dcfa486e7881b60337ce8f3532429fff4
Nikkon97 <you@example.com>
2023-10-15 21:13:34 +0300

    24.2 Serializers

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -13,3 +14,32 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Payment(models.Model):
+    PAY_TYPE_CARD = 'card'
+    PAY_TYPE_CASH = 'cash'
+
+    PAY_TYPES = (
+        (PAY_TYPE_CASH, 'наличные'),
+        (PAY_TYPE_CARD, 'перевод на счет')
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='пользователь',
+                             on_delete=models.CASCADE)
+    datetime = models.DateTimeField(verbose_name='дата оплаты', **NULLABLE)
+
+    lesson = models.ForeignKey('materials.Lesson', on_delete=models.CASCADE,
+                               verbose_name='урок', **NULLABLE)
+    course = models.ForeignKey('materials.Course', on_delete=models.CASCADE,
+                               verbose_name='курс', **NULLABLE)
+
+    amount = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='сумма оплаты')
+    pay_type = models.CharField(choices=PAY_TYPES, default=PAY_TYPE_CASH, max_length=10, verbose_name='способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} - {self.amount}'
+
+    class Meta:
+        verbose_name = 'оплата'
+        verbose_name_plural = 'оплата'
